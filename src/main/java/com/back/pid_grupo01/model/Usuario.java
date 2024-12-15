2b0b96ecf98b03111aed91730df235bfd6af7a4b
Rusalka21 <gonzalesm.sergio@gmail.com>
2024-12-15 16:52:47 -0500

    user_seq fix

--- src/main/java/com/back/pid_grupo01/model/Usuario.java
+++ src/main/java/com/back/pid_grupo01/model/Usuario.java
@@ -4,15 +4,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,7 +25,7 @@ import lombok.NoArgsConstructor;
 })
 public class Usuario implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nombre;
